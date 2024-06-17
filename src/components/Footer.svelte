cb1b5efbdd37f744dd63ca18dd8e885425e48d3c
ared <chandyajin@gmail.com>
2024-06-17 19:04:24 +0530

    removed emoji

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -1,3 +1,11 @@
-<article>
-    <a href="https://github.com/0Ared/"> <h5> @0Ared </h5> </a>
-</article>
+<div class="footer">
+    <a href="https://github.com/0Ared/"> <p> <i>@0Ared</i></p> </a>
+</div>
+
+<style>
+    .footer {
+        margin: 0%;
+        display: flex;
+        justify-content: center;
+    }
+</style>
