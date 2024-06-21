521e85710e141adc0839348b66121b7a45d976d1
ared <chandyajin@gmail.com>
2024-06-21 16:58:20 +0530

    theme system

--- src/components/NavBar.svelte
+++ src/components/NavBar.svelte
@@ -1,6 +1,28 @@
+<script>
+    import { onMount } from "svelte";
+
+    let theme = "dark";
+    let html_tag;
+    onMount(() => {
+        html_tag = document.querySelector("html");
+    });
+
+    html_tag?.setAttribute("data-theme", theme);
+    function handleThemeSwitch() {
+        if (theme === "dark") {
+            html_tag?.setAttribute("data-theme", "light");
+            theme = "light";
+        } else {
+            html_tag?.setAttribute("data-theme", "dark");
+            theme = "dark";
+        }
+    }
+</script>
+
 <nav>
     <ul>
-        <li><strong>ARed</strong></li>
+        <li class="title"><strong on:click={handleThemeSwitch}>ARed</strong></li
+        >
     </ul>
     <ul>
         <li><a href="/" class="contrast">Home</a></li>
@@ -13,5 +35,9 @@
 <style>
     nav {
         margin: 20px;
+    }
+    .title strong {
+        font-stretch: extra-expanded;
+        font-size: 2.3rem;
     }
 </style>
